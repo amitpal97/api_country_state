aa0945031366692faf9d95f5f50016506808a4e3
amitpal97 <amitrpal9@gmail.com>
2022-12-14 13:36:05 +0530

    created index,js file and write logic

---
+++ index.js
@@ -0,0 +1,66 @@
+// // {first , second, third, four}=[one,two]=one=show
+// var country = {
+//   contName: {
+//     one: [{}],
+//     two: [{}],
+//   },
+//   second: {
+//     one: [{}],
+//     two: [{}],
+//   },
+//   third: {
+//     one: [{}],
+//     two: [{}],
+//   },
+//   four: {
+//     one: [{}],
+//     two: [{}],
+//   },
+// };
+
+// creating  a country variable.
+var country = {
+  // array of object for countries name
+  countries: [
+    {
+      countryName: ["india","Afghanistan"]
+    },
+    {
+      india: [
+        {
+          state: ["maharashatra", "aandra"],
+        },
+        {
+          cites: {
+            maharashatra: ["mumbai", "naviMumbai"],
+            aandra: ["demo", "demo1"],
+          },
+        },
+      ],
+    },
+
+    {
+      Afghanistan: [
+        {
+          state: ["Badakhshan", "Badghis", "Baghlan", "Balkh", "Bamian"],
+        },
+
+        {
+          cites: {
+            Badakhshan: ["demo", "demo1"],
+            Badghis: ["demo", "demo1"],
+          },
+        },
+      ],
+    },
+  ],
+};
+var countryName = document.querySelector(".countryName");
+var data = country.countries.forEach((elm,index,array)=>{
+    var name=console.log(elm.countryName);
+    // console.log(index);
+    // console.log(array);
+    countryName.innerHTML =name;
+    });
+    // console.log(data.countryName);
+
