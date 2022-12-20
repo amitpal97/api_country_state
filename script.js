e0c98d5b9117305153cb03f25a859a55160722c7
amitpal97 <amitrpal9@gmail.com>
2022-12-20 11:11:46 +0530

    click part all done and div will hide when click on another button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var wrapper = document.querySelector(".countryName");
 var wrapper1 = document.querySelector(".statesName");
 var wrapper2 = document.querySelector(".citesName");
-var statNotFo =document.querySelector(".statesNotFound");
+var statNotFo = document.querySelector(".statesNotFound");
 // var obj = [{}];
 
 var requestOptions = {
@@ -24,18 +24,18 @@ function setData(apiData) {
     // console.log(elm.dataset.iso2);
     // console.log(elm);
     elm.addEventListener("click", function () {
-     
-        fetch(
-          `https://api.countrystatecity.in/v1/countries/${elm.dataset.iso2}/states`,
-          requestOptions
-        )
+      fetch(
+        `https://api.countrystatecity.in/v1/countries/${elm.dataset.iso2}/states`,
+        requestOptions
+      )
         .then((res) => res.json())
 
         .then((result) => {
           // console.log(elm);
-          if(result.length > 0) { 
+          if (result.length > 0) {
+            var wrapperDiv1 = "";
             for (var i = 0; i < result.length; i++) {
-              var wrapperDiv1 = `<div data-state_code="${result[i].iso2}" class="statesNew">${result[i].name}</div>`;
+              var x = `<div data-state_code="${result[i].iso2}" class="statesNew">${result[i].name}</div>`;
               //  result[i].toString();
               //  console.log(result[i]);
               // if (result[i].length == 0) {
@@ -43,14 +43,13 @@ function setData(apiData) {
               // } else {
               //   console.log(result[i]);
               // }
-              wrapper1.innerHTML += wrapperDiv1
+              wrapperDiv1 += x;
             }
+            wrapper1.innerHTML = wrapperDiv1;
             // wrapper1.append(wrapperDiv1);
-
           } else {
             wrapper1.innerHTML = "Not found";
           }
-          
 
           document.querySelectorAll(".statesNew").forEach((elem) => {
             // console.log(elem);
@@ -68,19 +67,19 @@ function setData(apiData) {
               )
                 .then((res) => res.json())
                 .then((resultNew) => {
-                  if(resultNew.length > 0) { 
-
-                  for (var i = 0; i < resultNew.length; i++) {
-                    var wrapperDiv2 = `<div data-state_code="${resultNew[i].state_code}" class="citesNew">${resultNew[i].name}</div>`;
-                    // console.log(resultNew[i]);
-                    document.querySelectorAll(".citesNew");
-                    // wrapper2.innerHTML += wrapperDiv2;
+                  if (resultNew.length > 0) {
+                    var wrapperDiv2="";
+                    for (var i = 0; i < resultNew.length; i++) {
+                      var y = `<div data-state_code="${resultNew[i].state_code}" class="citesNew">${resultNew[i].name}</div>`;
+                      // console.log(resultNew[i]);
+                      document.querySelectorAll(".citesNew");
+                      wrapperDiv2 += y;
+                    }
+                    wrapper2.innerHTML = wrapperDiv2;
+                    // wrapper2.appendChild(wrapperDiv2);
+                  } else {
+                    wrapper2.innerHTML = "notFound";
                   }
-                  wrapper2.appendChild(wrapperDiv2);
-                }else{
-                  wrapper2.innerHTML += "notFound";
-
-                }
                   // document.querySelectorAll(".citesNew").forEach((elm) => {
                   //   // console.log(elm);
                   //   fetch(
